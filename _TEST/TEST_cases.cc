882730d6e0709c14502198d2a828e0c85233a00d
Nasseef Abukamail <abukamai@ohio.edu>
2019-01-20 21:16:34 -0500

    added senario, and moved testing to _TEST

---
+++ _TEST/TEST_cases.cc
@@ -0,0 +1,60 @@
+/*
+ * Unit testing for the Rectangle class
+ */
+//#define CATCH_CONFIG_RUNNER
+#define CATCH_CONFIG_MAIN  // Catch supplies a main program
+#include "../rectangle.h"
+#include "catch.hpp"
+
+TEST_CASE("Testing Rectangle constructors") {
+    Rectangle r;
+    REQUIRE(r.getWidth() == 1);
+    REQUIRE(r.getLength() == 1);
+    Rectangle r2(10, 5);
+    REQUIRE(r2.getLength() == 10);
+    REQUIRE(r2.getWidth() == 5);
+}
+TEST_CASE("Testing Rectangle getters/setters") {
+    Rectangle r(10, 5);
+    SECTION("Test getters") {
+        REQUIRE(r.getWidth() == 5);
+        REQUIRE(r.getLength() == 10);
+    }
+    SECTION("Test setters") {
+        r.setWidth(2);
+        r.setLength(8);
+        REQUIRE(r.getWidth() == 2);
+        REQUIRE(r.getLength() == 8);
+        r.setWidth(-3);
+        REQUIRE(r.getWidth() == 1);
+        REQUIRE(r.getLength() == 8);
+    }
+}
+
+TEST_CASE("Testing Rectangle Area") {
+    Rectangle r(10, 5);
+    REQUIRE(r.area() == 50);
+}
+
+TEST_CASE("Testing Overloaded ==") {
+    Rectangle r1(5, 6), r2(5, 6), r3;
+    REQUIRE(r1 == r2);
+    REQUIRE_FALSE(r1 == r3);
+}
+
+TEST_CASE("Rectangles with default dimensions") {
+    GIVEN("A default rectangle") {
+        Rectangle r;
+        REQUIRE(r.getLength() == 1);
+        REQUIRE(r.getWidth() == 1);
+        REQUIRE(r.area() == 1.0);
+
+        WHEN("length is doubled") {
+            r.setLength(r.getLength() * 2);
+
+            THEN("The area is also doubled") {
+                REQUIRE(r.area() == 2);
+            }
+        }
+    }
+}
